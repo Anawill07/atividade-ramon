0546d212afdce83a20412e731c4bb84be9a2b622
Anawill07 <162714291+Anawill07@users.noreply.github.com>
2024-09-24 09:51:16 -0300

    Add files via upload

--- lista de atividade/script3.js
+++ lista de atividade/script3.js
@@ -10,14 +10,38 @@ document.getElementById('bt_tarefa').addEventListener('click', addTarefa)
 
 //quando remover uma tarefa, remover do local storage também
 
+let lista_tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+lista_tarefas.forEach(element => {
+    criarElemento(element)
+    
+});
+
+
+
 function addTarefa(){
     //pegar o valor de dentro do input e armazenar em uma variavel
     const inputTarefa = document.getElementById('input_tarefa').value
     //verificar o valor do input e dar um alert se estiver vazio
     if (!inputTarefa) {
         alert('o input esta vazio')
+        return
     }
-    //criar o elemento <li>
+    if (lista_tarefas.includes (inputTarefa)) {
+        alert('não pode repitir')
+        return
+    }
+    // adiciona tarefa no array de tarefas
+    lista_tarefas.push(inputTarefa)
+
+    //tranforma o array em string e coloca a lista de tarefas no local storage
+    localStorage.setItem('tarefas', JSON.stringify(lista_tarefas))
+    criarElemento(inputTarefa)
+}
+
+    function criarElemento(inputTarefa){
+        document.getElementById('input_tarefa').value = ""
+        //criar o elemento <li>
     const elemento_tarefa = document.createElement('li')
     elemento_tarefa.className = 'item_tarefa'
     //adicionar o texto do input no novo <li>
@@ -25,9 +49,15 @@ function addTarefa(){
     conteudoTarefa.textContent = inputTarefa
     //adicionar um botão para deletar tarefa no novo <li>
     const botaoDeletar = document.createElement('button')
+
     botaoDeletar.textContent = 'Deletar Tarefa'
     botaoDeletar.addEventListener('click', () => {
+        //remove a tarefa da tela
         elemento_tarefa.remove()
+        //filtra a tarefa a ser removida
+        lista_tarefas = lista_tarefas.filter((tarefa) => tarefa !== inputTarefa)
+        // salva novamente a lista de tarefas no local storage
+        localStorage.setItem('tarefas', JSON.stringify(lista_tarefas))
     })
     //adicionar um botão para completar a tarefa no <li>
     const botaoCompletar = document.createElement('button')
@@ -41,3 +71,7 @@ function addTarefa(){
     elemento_tarefa.appendChild(botaoCompletar)
     document.getElementById('lista_tarefas').appendChild(elemento_tarefa)
 }
+
+
+
+
